4b9d2d2836bcaa64247551b90072bb869a503186
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-04-12 23:59:01 -0700

    added css combine task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,58 +7,71 @@ const gulpIf = require('gulp-if')
 const cssnano = require('gulp-cssnano')
 const imagemin = require('gulp-imagemin')
 const cache = require('gulp-cache')
+const concat = require('gulp-concat')
 const del = require('del')
 const runSequence = require('run-sequence')
 
-gulp.task('sass', function () {
-    return gulp.src('app/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+gulp.task('sass', function() {
+  return gulp
+    .src('app/scss/**/*.scss')
+    .pipe(concat('all.scss'))
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    )
 })
 
-gulp.task('browserSync', function () {
-    browserSync.init({
-        server: {
-            baseDir: 'app'
-        },
-    })
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  })
 })
 
-gulp.task('useref', function () {
-    return gulp.src('app/*.html')
-        .pipe(useref())
-        .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulpIf('*.css', cssnano()))
-        .pipe(gulp.dest('dist'))
+gulp.task('useref', function() {
+  return gulp
+    .src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
 })
 
-gulp.task('images', function () {
-    return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
-        .pipe(cache(imagemin({
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/images'))
+gulp.task('images', function() {
+  return gulp
+    .src('app/images/**/*.+(png|jpg|gif|svg)')
+    .pipe(
+      cache(
+        imagemin({
+          interlaced: true
+        })
+      )
+    )
+    .pipe(gulp.dest('dist/images'))
 })
 
-gulp.task('fonts', function () {
-    return gulp.src('app/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'))
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'))
 })
 
-gulp.task('clean:dist', function () {
-    return del.sync('dist');
+gulp.task('clean:dist', function() {
+  return del.sync('dist')
 })
 
-gulp.task('watch', function () {
-    gulp.watch('app/scss/**/*.scss', gulp.series('sass'))
-    gulp.watch('app/scss/**/*.scss', gulp.series('browserSync'))
-    gulp.watch('app/*.html').on('change', browserSync.reload)
-    gulp.watch('app/js/**/*.js').on('change', browserSync.reload)
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.scss', gulp.series('sass'))
+  gulp.watch('app/scss/**/*.scss', gulp.series('browserSync'))
+  gulp.watch('app/*.html').on('change', browserSync.reload)
+  gulp.watch('app/js/**/*.js').on('change', browserSync.reload)
 })
 
-gulp.task('default', gulp.series('sass', 'useref', 'images', 'fonts', function (done) {
-    done();
-}));
+gulp.task(
+  'default',
+  gulp.series('sass', 'useref', 'images', 'fonts', function(done) {
+    done()
+  })
+)
